6befde687bda67b9c55b21102137128c7ccf3e37
Adriel Fung <AdrielF31@gmail.com>
2022-06-27 04:45:27 -0400

    Fixed Bugs

--- login.py
+++ login.py
@@ -125,4 +125,19 @@ def linkedin_main(JobName, n):
 
     print("SUCCESS")
 
+def htmlformatter():
+    with open('replace.txt', 'r') as file:
+        replacer = file.read()
+
+    # Read in the file
+    with open('Table.html', 'r') as file :
+        filedata = file.read()
+
+    # Replace the target string
+        filedata = filedata.replace('<table>', replacer)
+
+    # Write the file out again
+    with open('Table.html', 'w') as file:
+        file.write(filedata)
+
 linkedin_main("Publishing", 3)
